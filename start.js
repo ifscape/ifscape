4542bde705a55d69ebf030db3cdf3ad234a6b434
mariastahelin <55252326+mariastahelin@users.noreply.github.com>
2019-11-11 17:26:43 -0300

    Update start.js

--- start.js
+++ start.js
@@ -1,11 +1,11 @@
 import {
-    fase1
+  fase1
 } from "./fase1.js";
 import {
-    fase2
+  fase2
 } from "./fase2.js";
 export {
-    start
+  start
 
 };
 
@@ -14,43 +14,43 @@ var setupSceneInput;
 var start = new Phaser.Scene("SceneA");
 
 start.preload = function () {
-    this.load.image("sky", "assets/sky.png");
-    this.load.spritesheet("start", "assets/start.png", {
-        frameWidth: 200,
-        frameHeight: 200
-    });
+  this.load.image("sky", "assets/sky.png");
+  this.load.spritesheet("start", "assets/start.png", {
+    frameWidth: 200,
+    frameHeight: 200
+  });
 };
 
 
 start.create = function () {
-    this.add.image(400, 300, "sky");
+  this.add.image(400, 300, "sky");
 
-    this.anims.create({
-        key: "animacao",
-        frames: this.anims.generateFrameNumbers("start", {
-          start: 0,
-          end: 29
-        }),
-        frameRate: 10,
-        repeat: -1
-      });
+  this.anims.create({
+    key: "animacao",
+    frames: this.anims.generateFrameNumbers("start", {
+      start: 0,
+      end: 29
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
 
 
-    //this.add.sprite(400, 300, "start");
-    button = this.add
-        .sprite(400, 300, "start", 0)
-        //.setOrigin(1, 0)
-        .setInteractive();
+  //this.add.sprite(400, 300, "start");
+  button = this.add
+    .sprite(400, 300, "start", 0)
+    //.setOrigin(1, 0)
+    .setInteractive();
 
-    button.on(
-        "pointerup",
-        function () {
-            this.scene.start(fase1);
-        },
-        this
-    );
+  button.on(
+    "pointerup",
+    function () {
+      this.scene.start(fase1);
+    },
+    this
+  );
 };
 
 start.update = function () {
-    button.anims.play("animacao", true);
+  button.anims.play("animacao", true);
 }
