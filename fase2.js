06c6cd30ff00d2eb731818144a4b0fe36556974d
mariastahelin <55252326+mariastahelin@users.noreply.github.com>
2019-11-25 17:30:53 -0300

    Update fase2.js

--- fase2.js
+++ fase2.js
@@ -8,6 +8,7 @@ import {
 var fase2 = new Phaser.Scene("fase2");
 
 var player;
+var player2;
 var stars;
 var bombs;
 var platforms;
@@ -17,6 +18,8 @@ var score = 0;
 var gameOver = false;
 var scoreText;
 var button;
+var keyA, keyD, keyW;
+var playerGot;
 
 
 fase2.preload = function () {
@@ -59,6 +62,15 @@ fase2.create = function () {
   //  Player physics properties. Give the little guy a slight bounce.
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
+
+  player2 = this.physics.add.sprite(700, 450, 'dude');
+  player2.setScale(1);
+  player2.setSize(27, 32, true);
+  
+
+  //  Player physics properties. Give the little guy a slight bounce.
+  player2.setBounce(0.2);
+  player2.setCollideWorldBounds(true);
 
   //  Our player animations, turning, walking left and walking right.
   this.anims.create({
@@ -103,6 +115,10 @@ fase2.create = function () {
   //  Input Events
   cursors = this.input.keyboard.createCursorKeys();
 
+  keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+  keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+  keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
   //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
   stars = this.physics.add.group({
     key: 'star',
@@ -120,7 +136,34 @@ fase2.create = function () {
     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
 
   });
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', {
+      start: 0,
+      end: 3
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
 
+  this.anims.create({
+    key: 'turn',
+    frames: [{
+      key: 'dude',
+      frame: 4
+    }],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', {
+      start: 5,
+      end: 8
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
   //
   bombs = this.physics.add.group();
 
@@ -128,17 +171,17 @@ fase2.create = function () {
   var bomb1 = bombs.create(x, 16, 'bomb');
 
   var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-  var bomb2 = bombs.create(x, 16, 'bomb');
+  //var bomb2 = bombs.create(x, 16, 'bomb');
 
   bomb1.setBounce(1);
   bomb1.setCollideWorldBounds(true);
   bomb1.setVelocity(Phaser.Math.Between(-200, 200), 20);
   bomb1.allowGravity = false
 
-  bomb2.setBounce(1);
-  bomb2.setCollideWorldBounds(true);
-  bomb2.setVelocity(Phaser.Math.Between(-200, 200), 20);
-  bomb2.allowGravity = false
+  //bomb2.setBounce(1);
+  //bomb2.setCollideWorldBounds(true);
+  //bomb2.setVelocity(Phaser.Math.Between(-200, 200), 20);
+  //bomb2.allowGravity = false
   //
   lava = this.physics.add.sprite(395, 800, "lava");
   lava.setCollideWorldBounds(true);
@@ -157,14 +200,19 @@ fase2.create = function () {
   this.physics.add.collider(stars, platforms);
   this.physics.add.collider(bombs, platforms);
   this.physics.add.collider(lava, platforms);
+  this.physics.add.collider(player2, platforms);
 
 
   //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
   this.physics.add.overlap(player, stars, collectStar, null, this);
+  this.physics.add.overlap(player2, stars, collectStar, null, this);
 
   // a bomba nao pega no personagem
   this.physics.add.collider(player, bombs, hitBomb, null, this);
   this.physics.add.collider(player, lava, hitLava, null, this);
+
+  this.physics.add.collider(player2, bombs, hitBomb, null, this);
+  this.physics.add.collider(player2, lava, hitLava, null, this);
 
   const fullscreenButton = this.add
     .image(this.scale.width - 16, 16, "fullscreen", 0)
@@ -206,35 +254,46 @@ fase2.update = function () {
     player.setVelocityY(-330);
   }
 
+  if (keyA.isDown) {
+    player2.setVelocityX(-160);
+  
+    player2.anims.play('left', true);
+  } else if (keyD.isDown) {
+    player2.setVelocityX(160);
+  
+    player2.anims.play('right', true);
+  } else {
+    player2.setVelocityX(0);
+  
+    player2.anims.play('turn');
+  }
+  
+  if (keyW.isDown && player2.body.touching.down) {
+    player2.setVelocityY(-330);
+  }
+
   lava.anims.play("lava", true)
 }
 
+
+
+
+
 function collectStar(player, star) {
-  star.disableBody(true, true);
+  //
+  if(playerGot !== player) {
+    star.disableBody(true, true);
 
-  //  Add and update the score
-  score += 10;
-  scoreText.setText('Score: ' + score);
+    score += 10;
+    scoreText.setText('Score: ' + score);
 
-  if (stars.countActive(true) === 0) {
-    //  A new batch of stars to collect
-    stars.children.iterate(function (child) {
-
-      child.enableBody(true, child.x, 0, true, true); 
-    
-    });
+    if (stars.countActive(true) === 0 ) {
+      this.scene.start(fase3);
+    }
   }
-    //
-    this.scene.start(fase3);
-  
-    var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-    var bomb = bombs.create(x, 16, 'bomb');
-    bomb.setBounce(1);
-    bomb.setCollideWorldBounds(true);
-    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-    bomb.allowGravity = false
-  }
+  playerGot = player
+  //
+}
   
 
 function hitBomb(player, bomb) {
@@ -247,6 +306,6 @@ function hitBomb(player, bomb) {
 function hitLava(player, lava) {
   this.physics.pause();
   player.setTint(0xff0000);
-  player.anims.play('animacao');
+  player.anims.play('turn');
   gameOver = true;
 }
